e2cc07fc97e3d78bbf0624d61857e717a02ab42b
coreylin <coreylin@tencent.com>
2023-06-30 12:36:22 +0800

    update by fast_commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # script_installer
 一个存放常见的Shell安装脚本的项目，旨在为用户提供一键安装各种常见工具和软件的便捷方式。该项目汇集了多个常见工具和软件的安装脚本，用户可以通过从网络下载并执行这些脚本，轻松地完成工具和软件的安装过程。
+
+## create_readme
+- 说明
+  
+为了解决在 linux 服务器上创建多个服务，工作目录等内容后，容易忘记服务位置，以及工作目录混乱，同时也方便服务交接，因此将服务器上的一些关键的服务信息放到公共文件中，便于管理，并通过 readme 和 vireadme 进行查看和编辑
+
+- 命令
+
+```shell
+curl -sLk https://raw.githubusercontent.com/coreylam/script_installer/main/create_readme.sh | sh
+```
+
--- create_readme.sh
+++ create_readme.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
 # 获取命令行输入的路径
-path=$1
+read -p "Please enter a path (default is ~): " path
+
+# If the user didn't enter a path, use ~ as the default path
 if [ -z "$path" ]; then
-    path=~
+    path="~"
 fi
+echo "The path entered is: $path"
 
 # 在指定路径下创建.readme.md文件
 touch "$path/.readme.md"
-
 # 添加alias
+echo "添加 reame, vireadme 命令到 ~/.bashrc"
 echo "alias readme='cat $path/.readme.md'" >> ~/.bashrc
 echo "alias vireadme='vim $path/.readme.md'" >> ~/.bashrc
 
 # 使alias立即生效
 source ~/.bashrc
 
-echo "创建.readme.md文件并添加alias完成！ 通过 readme 命令查看本机的服务说明， vireadme 查看"
+echo "创建.readme.md文件并添加alias完成！\n通过 readme 命令查看本机的服务说明， vireadme 查看"
